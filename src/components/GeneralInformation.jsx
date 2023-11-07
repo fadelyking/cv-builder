4e47cfb9c1f68a4bbe7a2160441148b7dab1dbcb
fadelyking <fadely195@gmail.com>
2023-11-07 09:30:39 +0300

    formatting

--- src/components/GeneralInformation.jsx
+++ src/components/GeneralInformation.jsx
@@ -1,36 +1,33 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function ParentFunction(props) {
-    const [name, setName] = useState("")
-    const [email, setEmail] = useState("")
-    const [number, setNumber] = useState(0)
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [number, setNumber] = useState(0);
 
-    function handleNameChange(e) {
-        setName(e.target.value);
-    }
+	function handleNameChange(e) {
+		setName(e.target.value);
+	}
 
-    function handleEmailChange(e) {
-        setEmail(e.target.value);
-    }
+	function handleEmailChange(e) {
+		setEmail(e.target.value);
+	}
 
-    function handleNumberChange(e) {
-        setNumber(e.target.value)
+	function handleNumberChange(e) {
+		setNumber(e.target.value);
+	}
 
-    }
+	function handleSubmit(e) {
+		e.preventDefault();
+		props.onSubmit({ name, email, number });
+	}
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        props.onSubmit({name, email, number});
-    }
-
-    return (
-        <form onSubmit={handleSubmit}>
-        <input label="Name" type="text" value={name} onChange={handleNameChange}/> 
-        <input label="Email" type="email" value={email} onChange={handleEmailChange}/>
-        <input label="Number" type="number" value={number} onChange={handleNumberChange}/>
-        <input type="Submit" />
-
-        </form> 
-    )
+	return (
+		<form onSubmit={handleSubmit}>
+			<input label="Name" type="text" value={name} onChange={handleNameChange} />
+			<input label="Email" type="email" value={email} onChange={handleEmailChange} />
+			<input label="Number" type="number" value={number} onChange={handleNumberChange} />
+			<input type="Submit" />
+		</form>
+	);
 }
-
