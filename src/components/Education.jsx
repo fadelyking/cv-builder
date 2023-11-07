f1a9331b0b3f8b9b923db29d2219b28058233ad7
fadelyking <fadely195@gmail.com>
2023-11-07 09:29:43 +0300

    education form

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,36 +1,38 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function EducationInfo(props) {
-    const [school, setSchool] = useState("")
-    const [titleOfStudy, setTitleOfStudy] = useState("")
-    const [date, setDate] = useState(0)
+	const [school, setSchool] = useState("");
+	const [titleOfStudy, setTitleOfStudy] = useState("");
+	const [date, setDate] = useState(0);
 
-    function handleSchoolChange(e) {
-        setSchool(e.target.value);
-    }
+	function handleSchoolChange(e) {
+		setSchool(e.target.value);
+	}
 
-    function handleStudyChange(e) {
-        setTitleOfStudy(e.target.value);
-    }
+	function handleStudyChange(e) {
+		setTitleOfStudy(e.target.value);
+	}
 
-    function handleDateChange(e) {
-        setDate(e.target.value)
+	function handleDateChange(e) {
+		setDate(e.target.value);
+	}
 
-    }
+	function handleSubmit(e) {
+		e.preventDefault();
+		props.onSubmit({ school, titleOfStudy, date });
+	}
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        props.onSubmit({school, titleOfStudy, date});
-    }
-
-    return (
-        <form onSubmit={handleSubmit}>
-        <input label="School Name" type="text" value={school} onChange={handleSchoolChange}/> 
-        <input label="Title of Study" type="text" value={titleOfStudy} onChange={handleStudyChange}/>
-        <input label="Date" type="date" value={date} onChange={handleDateChange}/>
-        <input type="Submit" />
-
-        </form> 
-    )
+	return (
+		<form onSubmit={handleSubmit}>
+			<input label="School Name" type="text" value={school} onChange={handleSchoolChange} />
+			<input
+				label="Title of Study"
+				type="text"
+				value={titleOfStudy}
+				onChange={handleStudyChange}
+			/>
+			<input label="Date" type="date" value={date} onChange={handleDateChange} />
+			<input type="Submit" />
+		</form>
+	);
 }
-
